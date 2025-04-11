d47728afc3ac928b5dbe33837bfa7ef862ab1cb9
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2025-04-11 21:00:10 +0200

    update update_version.py

--- update_version.py
+++ update_version.py
@@ -5,11 +5,13 @@ from pathlib import Path
 
 
 # RegEx to find versions
-VERSION_INIT_REGEX: re.Pattern[str] = re.compile(r"^(__version__\s*=\s*[\'\"])([^\'\"]*)([\'\"])", re.MULTILINE)
+VERSION_INIT_REGEX: re.Pattern[str] = re.compile(r"^(__version__\s*=\s*__version__\s*#\s*)(\S+)(.{0})$", re.MULTILINE)
+VERSION_CNST_REGEX: re.Pattern[str] = re.compile(r"^(__version__\s*=\s*[\'\"])([^\'\"]*)([\'\"])", re.MULTILINE)
 VERSION_TOML_REGEX: re.Pattern[str] = re.compile(r"^(version\s*=\s*[\'\"])([^\'\"]*)([\'\"])", re.MULTILINE)
 
 # Paths to files to update
 INIT_PATH: Path = Path(__file__).parent / "py_nasa_api/__init__.py"
+CNST_PATH: Path = Path(__file__).parent / "py_nasa_api/client/const.py"
 TOML_PATH: Path = Path(__file__).parent / "pyproject.toml"
 
 
@@ -18,26 +20,34 @@ if not INIT_PATH.is_file():
     print(f"Invalid path for __init__.py: {INIT_PATH}", file=sys.stderr)
     sys.exit(1)
 
+if not CNST_PATH.is_file():
+    print(f"Invalid path for const.py: {CNST_PATH}", file=sys.stderr)
+    sys.exit(1)
+
 if not TOML_PATH.is_file():
-    print(f"Invalid path for pyproject.toml: {INIT_PATH}", file=sys.stderr)
+    print(f"Invalid path for pyproject.toml: {TOML_PATH}", file=sys.stderr)
     sys.exit(1)
 
 # Get file contents
 init_text = INIT_PATH.read_text("utf-8", "ignore")
+cnst_text = CNST_PATH.read_text("utf-8", "ignore")
 toml_text = TOML_PATH.read_text("utf-8", "ignore")
 
 # Extract the current version
 init_version_match = VERSION_INIT_REGEX.search(init_text)
+cnst_version_match = VERSION_CNST_REGEX.search(cnst_text)
 toml_version_match = VERSION_TOML_REGEX.search(toml_text)
 
 init_version = init_version_match.group(2) if init_version_match is not None else None
+cnst_version = cnst_version_match.group(2) if cnst_version_match is not None else None
 toml_version = toml_version_match.group(2) if toml_version_match is not None else None
 
 
 # Check for consistency
-if init_version != toml_version:
+if init_version != cnst_version or init_version != toml_version:
     print(
-        f"Conflicting versions found in __init__.py and pyproject.toml: {init_version} | {toml_version}",
+        f"Conflicting versions found in __init__.py, const.py and pyproject.toml: "
+        f"{init_version} | {cnst_version} | {toml_version}",
         file=sys.stderr,
     )
     sys.exit(1)
@@ -57,10 +67,12 @@ def _replacement_formatter(match: re.Match[str]) -> str:
 
 # Create replacement for previous match containing the new version
 init_replacement = VERSION_INIT_REGEX.sub(_replacement_formatter, init_version_match.group(0))
+cnst_replacement = VERSION_CNST_REGEX.sub(_replacement_formatter, cnst_version_match.group(0))
 toml_replacement = VERSION_TOML_REGEX.sub(_replacement_formatter, toml_version_match.group(0))
 
 # Save updated versions
 INIT_PATH.write_text(init_text.replace(init_version_match.group(0), init_replacement), "utf-8", "ignore")
+CNST_PATH.write_text(cnst_text.replace(cnst_version_match.group(0), cnst_replacement), "utf-8", "ignore")
 TOML_PATH.write_text(toml_text.replace(toml_version_match.group(0), toml_replacement), "utf-8", "ignore")
 
 print(f"Successfully updated version from {init_version} to {new_version}.")
